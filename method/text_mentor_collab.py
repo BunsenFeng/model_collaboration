602ffb230afcb4f7eb9827963e27d30f549f0cd6
haojinw0027 <seanwang027@gmail.com>
2025-12-10 03:45:41 +0000

    [mentor-collab] Haojin updates mentorcollab-free method.

---
+++ method/text_mentor_collab.py
@@ -0,0 +1,50 @@
+import json
+from data import eval
+from utils.mentor_collab import MentorCollab
+
+def run_method(task, task_type, gpu_ids, model_names, hyperparameters):
+    generator = hyperparameters.get("generator")
+    mentor = hyperparameters.get("mentor")
+    generator_devices = hyperparameters.get("generator_devices")
+    mentor_devices = hyperparameters.get("mentor_devices")
+    decision_proportion = hyperparameters.get("decision_proportion", 0.25)
+    patch_size = hyperparameters.get("patch_size", 16)
+    max_new_tokens = hyperparameters.get("max_response_length")
+    
+    mode = hyperparameters.get("mode", "free")
+    if mode == "train":
+        raise NotImplementedError("Training-based mode is not implemented yet.")
+    
+    mentor_collab = MentorCollab(generator, mentor, generator_devices, mentor_devices, decision_proportion, patch_size)
+    test_input_list = eval.prepare_inputs(task, task_type, "test")
+    outputs = []
+    for input in test_input_list:
+        output = mentor_collab.generate(input, max_new_tokens)
+        outputs.append(output)
+    
+    test_scores = eval.get_scores(task, task_type, "test", outputs)
+    avg_test_scores = sum(test_scores) / len(test_scores)
+    print("Final test {} score after logit fusion: {}".format(task, avg_test_scores))
+    
+    experiment_logs = {
+        "task": task,
+        "task_type": task_type,
+        "model_names": model_names,
+        "hyperparameters": hyperparameters,
+        "avg_test_score": avg_test_scores,
+        "logs": []
+    }
+    for i in range(len(test_input_list)):
+        log = {
+            "input": test_input_list[i],
+            "output": outputs[i],
+            "score": test_scores[i]
+        }
+        experiment_logs["logs"].append(log)
+
+    # file name with task, number of models, and avg_test_score with 4 decimal places
+    log_filename = "logs/{}_{}_{}_mentor_collab.json".format(task, len(model_names), round(avg_test_scores, 4))
+    with open(log_filename, "w") as f:
+        json.dump(experiment_logs, f, indent=4)
+
+    return 0
